a8dfcb4b3aac71df296228e8068f8ad2685bdc90
Felipe Delfino <felipe.delf11@gmail.com>
2026-08-18 15:14:52 -0300

    tests: teste de contexto e teste unitario para validar a estrutura

---
+++ src/main/java/br/com/jmusicbot/domain/Calculadora.java
@@ -0,0 +1,11 @@
+package br.com.jmusicbot.domain;
+
+public class Calculadora {
+
+    public int calculo(int primeiroNumero, int segundoNumero) {
+
+        int soma = primeiroNumero + segundoNumero;
+        return soma;
+    }
+
+}
